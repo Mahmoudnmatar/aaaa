1e6e56054b60e98eb93ed07ef4432fba5715646a
Matar <matar72016@gmail.com>
2024-12-03 19:03:15 +0200

    Assg24

---
+++ Assets/C#/RaceState.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assg24
+{
+    public enum RaceState
+    {
+
+        Start,
+        Accelerate,
+        Turn,
+        Crash,
+        Finish,
+
+
+    }
+}
